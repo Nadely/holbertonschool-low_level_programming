4fd6a049c4927bd77cfcd2061a929fc8f483f554
Nadely <135656686+Nadely@users.noreply.github.com>
2023-06-16 00:41:51 +0200

    Size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,5 +6,9 @@
 int main(void)
 {
 printf("\n %d% octets pour variable de type char ",sizeof(char));
+printf("\n %d% octets pour variable de type int ",sizeof(int));
+printf("\n %d% octets pour variable de type long int ",sizeof(long int));
+printf("\n %d% octets pour variable de type long long int ",sizeof(long long int));
+printf("\n %d% octets pour variable de type float ",sizeof(float));
 return (0);
 }
