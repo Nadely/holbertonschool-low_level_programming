f525fca9aac49363f3d382cfe827171653ffa780
Nadely <135656686+Nadely@users.noreply.github.com>
2023-06-16 08:50:42 +0200

    Size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,5 +10,6 @@ printf("\nSize of an int: %d byte(s)", sizeof(int));
 printf("\nSize of a long int: %d byte(s)", sizeof(long int));
 printf("\nSize of a long long int: %d byte(s)", sizeof(long long int));
 printf("\nSize of a float: %d byte(s)", sizeof(float));
+printf("\n");
 return (0);
 }
