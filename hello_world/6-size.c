7e37059243ee08504f1e3e715f63fa3eb0284101
Nadely <135656686+Nadely@users.noreply.github.com>
2023-06-16 08:39:33 +0200

    Size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,10 +5,10 @@
 */
 int main(void)
 {
-printf("\n %d Size of a char ", sizeof(char));
-printf("\n %d Size of a int ", sizeof(int));
-printf("\n %d Size of a long int ", sizeof(long int));
-printf("\n %d Size of a long long int ", sizeof(long long int));
-printf("\n %d Size of a float ", sizeof(float));
+printf("\n  Size of a char: "%d, sizeof(char));
+printf("\n  Size of a int: "%d, sizeof(int));
+printf("\n  Size of a long int: "%d, sizeof(long int));
+printf("\n  Size of a long long int: "%d, sizeof(long long int));
+printf("\n  Size of a float: "%d, sizeof(float));
 return (0);
 }
