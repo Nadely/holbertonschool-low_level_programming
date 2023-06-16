9da69649f326e68648453f2cf780a9812ad4dd68
Nadely <135656686+Nadely@users.noreply.github.com>
2023-06-16 08:49:03 +0200

    Update 6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,7 +6,7 @@
 int main(void)
 {
 printf("\nSize of a char: %d byte(s)", sizeof(char));
-printf("\nSize of an int: %d byte(s)", sizeof(int));
+printf("\nSize of aint: %d byte(s)", sizeof(int));
 printf("\nSize of a long int: %d byte(s)", sizeof(long int));
 printf("\nSize of a long long int: %d byte(s)", sizeof(long long int));
 printf("\nSize of a float: %d byte(s)", sizeof(float));
