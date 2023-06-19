d3cb1e9b992571f2a992b0e387f8acda61ffa8e9
Nadely <6890@holbertonschool.com>
2023-06-19 13:09:04 -0700

    Putchar 2

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - putchar command (2)
+* Return: putchar command (2)
+*/
+int main(void)
+{
+int az, AZ;
+for (az = 'a'; az <= 'z'; az++)
+for (AZ = 'A'; AZ <= 'Z'; AZ++)
+{
+putchar(az);
+}
+{
+putchar(AZ);
+}
+{
+putchar('\n');
+}
+return (0);
+}
