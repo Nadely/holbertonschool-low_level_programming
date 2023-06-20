bdc26c86605c3bd1f0293d6e56cece731e588b4f
Nadely <6890@holbertonschool.com>
2023-06-20 03:05:46 -0700

    Numbers must

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - Numbers must
+* Return: Numbers must
+*/
+int main(void)
+{
+int numbers;
+for (numbers = '0'; numbers <= '9'; numbers++)
+putchar(numbers,);
+putchar('\n');
+return (0);
+}
