2ef9fcfda4e19d383e47e00e801d74980b1d9acb
Nadely <6890@holbertonschool.com>
2023-06-20 01:21:53 -0700

    Putchar numbers

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Putchar numbers
+* Return: Putchar numbers
+*/
+int main(void)
+{
+int numbers;
+for (numbers = 0; numbers < 10; numbers++)
+{
+putchar(numbers);
+}
+{
+putchar(\n);
+}
+return (0);
+}
