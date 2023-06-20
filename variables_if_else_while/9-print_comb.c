07bd3c7cee771e7d2c1312ff1855c6bdc01927ec
Nadely <135656686+Nadely@users.noreply.github.com>
2023-06-20 14:09:27 +0200

    Must numbers

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -7,9 +7,14 @@ int main(void)
 {
 int numbers;
 for (numbers = '0'; numbers <= '9'; numbers++)
+{
 putchar(numbers);
+if (numbers && numbers)
 putchar(',');
 putchar(' ');
+}
+{
 putchar('\n');
+}
 return (0);
 }
