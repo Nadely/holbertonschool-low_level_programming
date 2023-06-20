f19a15ed3f651cef3b4d98b4a795afa905a3494e
Nadely <135656686+Nadely@users.noreply.github.com>
2023-06-20 12:12:46 +0200

    Must numbers

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -5,12 +5,10 @@
 */
 int main(void)
 {
-int numbers, comma;
+int numbers;
 for (numbers = '0'; numbers <= '9'; numbers++)
-for (comma = ',';)
 {
 putchar(numbers);
-putchar(comma);
 putchar('\n');
 }
 return (0);
