948dee17a9f8552d98fab10218d9b5999245b731
Nadely <6890@holbertonschool.com>
2023-06-20 02:04:38 -0700

    Reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Alphabet in revers
+* Return: Alphabet in reverse
+*/
+int main(void)
+{
+int za;
+for (za = 'z'; za >= 'a'; za++)
+{
+putchar(za);
+}
+{
+putchar('\n');
+}
+return (0)
+}
