d5ccc9969bb5ac2da82ffb1dd4f0a80a4a439d43
Nadely <6890@holbertonschool.com>
2023-06-20 01:08:03 -0700

    Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Print numbers
+* Return: Print numbers
+*/
+int main(void)
+{
+int numbers;
+for (numbers = 0; numbers < 10; numbers++)
+{
+puts("%d", numbers);
+}
+return (0)
+}
