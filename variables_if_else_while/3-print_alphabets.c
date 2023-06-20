e0f801c263ce619fad0ec16b24501e9c612c4974
Nadely <6890@holbertonschool.com>
2023-06-20 00:26:46 -0700

    Putchar (2)

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -6,8 +6,8 @@
 int main(void)
 {
 int az, AZ;
-for (az = 'a'; az <= 'z';)
-for (AZ = 'A'; AZ <= 'Z';)
+for (az = 'a'; az < 'z'; az = 'z')
+for (AZ = 'A'; AZ < 'Z'; AZ = 'Z')
 {
 putchar(az);
 putchar(AZ);
