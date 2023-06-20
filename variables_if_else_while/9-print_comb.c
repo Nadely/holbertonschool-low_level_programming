186fc2362a52031a0516a2c7e18e5c3d90f683b1
Nadely <135656686+Nadely@users.noreply.github.com>
2023-06-20 12:19:56 +0200

    Update 9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -6,12 +6,10 @@
 int main(void)
 {
 int numbers;
-for (numbers = '0'; numbers <= '9'; numbers++)
+for (numbers = '0, '; numbers <= '9, '; numbers++)
 putchar(numbers);
-{
 putchar(',');
 putchar(' ');
 putchar('\n');
-}
 return (0);
 }
