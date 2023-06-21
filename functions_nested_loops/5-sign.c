1d12cbed9ec83d7a656dbb6047a1e0c61dc7b534
Nadely <6890@holbertonschool.com>
2023-06-21 10:08:41 -0700

    Sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* _isalpha - sign of a numbers
+* Return: sign of a numbers
+* @n: sign
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+_putchar(',');
+return (1);
+}
+else if (n = 0)
+_putchar('0');
+_putchar(',');
+return (0);
+}
+{
+else if (n < 0)
+_putchar('-');
+_putchar(',');
+return (-1)
+}
+{
+return (0);
+}
+}
