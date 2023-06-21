f6666041269b26383f5c3e3be7eedd482a27dcd9
Nadely <6890@holbertonschool.com>
2023-06-21 07:53:02 -0700

    Isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _isalpha - lowercase or not
+* Return: lowercase or not
+*/
+int _isalpha(int c)
+{
+if (c)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+{
+return (0);
+}
+}
