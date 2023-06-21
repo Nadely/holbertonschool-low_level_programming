574999da3a686ce99134f2248b9164a14111b715
Nadely <6890@holbertonschool.com>
2023-06-21 02:37:06 -0700

    Third main

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print-alphabet_x10 - Print 10 alphabet
+ * Return: Print 10 alphabet
+ */
+void print_alphabet_x10(void)
+{
+int az;
+for (az = 'a'; az <= 'z'; az++)
+{
+_putchar(az);
+}
+{
+ _putchar('\n');
+}
+}
