ae22649f8812e99f02bfeebe2d055a07c1d08c37
Nadely <6890@holbertonschool.com>
2023-06-21 02:26:53 -0700

    Second main

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_alphabet - Print alphabet
+* Return: Print alphabet
+*/
+void print_alphabet(void)
+{
+int az;
+for (az = 'a'; az <= 'z'; az++)
+{
+_putchar(az);
+_putchar('\n');
+}
+}
