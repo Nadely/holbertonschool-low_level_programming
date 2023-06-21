49ffbda37817e227b5f9361ecb8cb41e71e28c71
Nadely <6890@holbertonschool.com>
2023-06-21 04:53:11 -0700

    Fourth main

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _islower - lowercase character
+* Return: lowercase character
+*/
+int _islower(int c)
+{
+int character = 'c'
+if (character = 'c')
+{
+_putchar(1)
+}
+else
+{
+_putchar(0)
+}
+return (0)
+}
