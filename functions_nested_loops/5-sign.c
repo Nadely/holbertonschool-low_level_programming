1c4bb6524dc4da0b4754f298aa6420f857f7d945
Nadely <135656686+Nadely@users.noreply.github.com>
2023-06-21 19:17:52 +0200

    Sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -12,14 +12,14 @@ _putchar('+');
 _putchar(',');
 return (1);
 }
-{
 else if (n == '0')
+{
 _putchar('0');
 _putchar(',');
 return (0);
 }
-{
 else if (n <'0')
+{
 _putchar('-');
 _putchar(',');
 return (-1);
