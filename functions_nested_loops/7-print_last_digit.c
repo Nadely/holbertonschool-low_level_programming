44bf900249d93961c4b66ca6ec495e06645e4b67
Nadely <135656686+Nadely@users.noreply.github.com>
2023-06-21 23:29:15 +0200

    Last

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * print_last_digit - last number
- * Return: last number
- * @x: last
- */
+* print_last_digit - last number
+* Return: last number
+* @x: last
+*/
 int print_last_digit(int x)
 {
-  _putchar(x);
-  return (0);
+_putchar(x);
+return (0);
 }
