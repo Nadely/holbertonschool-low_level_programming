348e9a0e7ec9b14d916d4b0532458e09879a6c71
Nadely <135656686+Nadely@users.noreply.github.com>
2023-06-22 15:29:05 +0200

    Tables

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -3,14 +3,15 @@
 * times_table - 9 table
 * Return: 9 table
 * @n: numbers
-*@o; numbers
+* @o: numbers
+* @r: resilt
 */
 void times_table(void)
 {
-int n, o;
+int n, o, r;
 for (n = 48; n <= 57; n++)
 for (o = 48; o <= 57; o++)
-return (n*o)
+r = n * o
 _putchar(n);
 _putchar (',');
 _putchar ('\n');
