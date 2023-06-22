86325bbee271709d156d39676762316eaf02b14b
Nadely <6890@holbertonschool.com>
2023-06-22 01:12:12 -0700

    Last

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -6,13 +6,8 @@
 */
 int print_last_digit(int x)
 {
-<<<<<<< HEAD
-  int l;
-  l = x % 10;
-  _putchar(l);
-  return (0);
-=======
-_putchar(x);
+int l;
+l = x % 10;
+_putchar(l);
 return (0);
->>>>>>> 44bf900249d93961c4b66ca6ec495e06645e4b67
 }
