3dd8f0ae0ad6037bb8f2ca858599f40f0b48ce16
Nadely <135656686+Nadely@users.noreply.github.com>
2023-06-22 15:18:49 +0200

    Clock

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -6,11 +6,23 @@
 void jack_bauer(void)
 {
 int h, f, m, n;
-for (h = 48; h <= 48; h++)
-for (f = 48; f <= 51; f++)
+for (h = 48; h <= 50; h++)
 for (m = 48; m <= 53; m++)
 for (n = 48; n <= 57; n++)
 {
+if(h = 50)
+{
+for (f = 48; f <= 51; f++)
+_putchar(h);
+_putchar(f);
+_putchar(':');
+_putchar(m);
+_putchar(n);
+_putchar('\n');
+}
+else if(h < 50)
+{
+for (f = 48; f <= 57; f++)
 _putchar(h);
 _putchar(f);
 _putchar(':');
