c1fdace97b7c8de4d161938242f7d2a5afa22782
Nadely <6890@holbertonschool.com>
2023-06-22 04:26:16 -0700

    Tables

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* times_table - 9 table
+* Return: 9 table
+*/
+void times_table(void)
+{
+int n;
+for (n = 48; n <= 57; n++)
+{
+return (n * (n));
+_putchar(n);
+_putchar ('\n');
+}
+}
