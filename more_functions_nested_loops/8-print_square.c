883b46e15c1c77ad0f0d1a1224553b3c88185751
Nadely <6890@holbertonschool.com>
2023-06-23 15:01:33 -0700

    Diagonal

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_square  - square line
+* Return: square line
+* @n: numbers
+*/
+void print_square(int size)
+{
+int x, y;
+for (x = 0; x <= size; x++)
+_putchar('\n');
+for (y = 0; y <= size; y++)
+_putchar(35);
+}
