904f49961fa429cf5cb9392ee67d1dbae3883045
Nadely <6890@holbertonschool.com>
2023-06-23 13:30:47 -0700

    More

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* more_numbers - again
+* Return: again
+*/
+void more_numbers(void)
+{
+int n, x;
+for (x = 0; x <= 10; x++)
+{
+for (n = 0; n <= 14; n++)
+{
+if (n >= 10)
+_putchar('0' + (n / 10));
+_putchar('0' + (n % 10));
+}
+_putchar('\n');
+}
+}
