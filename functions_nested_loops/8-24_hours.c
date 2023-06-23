ffaecabae9f71f0f3a70117879063bd49de351df
Nadely <135656686+Nadely@users.noreply.github.com>
2023-06-23 13:07:53 +0200

    Clock

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,8 +7,11 @@ void jack_bauer(void)
 {
 int h, f, m, n;
 for (h = '0'; h <= '2'; h++)
+{
 for (f = '0'; f <= '3'; f++)
+{
 for (m = '0'; m <= '5'; m++)
+{
 for (n = '0'; n <= '9'; n++)
 {
 _putchar(h);
@@ -17,5 +20,8 @@ _putchar(':');
 _putchar(m);
 _putchar(n);
 _putchar('\n');
+}
+}
+}
 }
 }
