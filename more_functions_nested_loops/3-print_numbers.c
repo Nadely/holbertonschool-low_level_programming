25bc0517cbe300b5f22fb5738dfafd47a16ac0b2
Nadely <6890@holbertonschool.com>
2023-06-23 12:10:57 -0700

    Print num

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,10 +2,10 @@
 /**
 * print_numbers - Print
 * Return: Print
+* @n: numbers
 */
-void print_numbers(void)
+void print_numbers(int n)
 {
-int n
 for (n = 0; n < = 9; n++)
 _putchar(n);
 _putchar('\n');
