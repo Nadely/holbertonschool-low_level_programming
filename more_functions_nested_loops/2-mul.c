2e921c715200eb2dccd2c14f2484cbb30a6bb6b9
Nadely <6890@holbertonschool.com>
2023-06-23 11:53:11 -0700

    Multiplies

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+# include "main.h"
+/**
+* mul - multiplies two integers
+* Return: multiplies two integers
+* @a: numbers
+* @b: numbers two
+*/
+int mul(int a, int b)
+{
+for (a * b)
+}
+{
+return (0)
+}
