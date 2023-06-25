bd787125eb8c0eadd80765042f8db3553331a238
Nadely <135656686+Nadely@users.noreply.github.com>
2023-06-25 15:06:44 +0200

    Square

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -9,13 +9,13 @@ void print_square(int size)
 int x, y;
 for (x = 0; x <= size; x++)
 {
-for (y = 0; x <= size; y++)
 if (size > 0)
 {
 _putchar(35);
-}
 _putchar('\n');
+}
 else if (size <= 0)
 _putchar('\n');
+for (y = 0; x <= size; y++)
 }
 }
