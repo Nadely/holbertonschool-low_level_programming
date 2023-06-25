f75570210149164025d46cc1c473fd292e176414
Nadely <135656686+Nadely@users.noreply.github.com>
2023-06-25 15:55:17 +0200

    Diagonal

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -14,10 +14,10 @@ _putchar('\n');
 else if (n > 0)
 for (x = 0; x <= n; x++)
 {
+for (y = 0; y <= x; y++)
+{
 _putchar(' ');
 _putchar(92);
 _putchar ('\n');
 }
-for (y = 0; y <= x; y++)
-_putchar(' ');
 }
