9076e596c57b4c7f620b127e461141254d153d2f
Nadely <135656686+Nadely@users.noreply.github.com>
2023-06-25 16:07:43 +0200

    Triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,12 +1,15 @@
 #include "main.h"
 /**
- * print_triangle - Triangle
- * Return: Triangle
- * @size: size
- */
+* print_triangle - Triangle
+* Return: Triangle
+* @size: size
+*/
 void print_triangle(int size)
 {
 int x, y;
+if (size <= 0)
+_putchar('\n');
+else
 for (x = 0; x <= size; x++)
 {
 for (y = 0; y <= size; y++)
@@ -15,7 +18,5 @@ _putchar(' ');
 _putchar(35);
 _putchar('\n');
 }
-if (size <= 0)
-_putchar('\n');
 }
 }
