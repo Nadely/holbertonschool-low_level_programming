ffbadcf70a9d93c3e1049712f3c6ff52a7038d2b
Nadely <135656686+Nadely@users.noreply.github.com>
2023-06-25 15:17:44 +0200

    Diagonal

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,7 +8,6 @@ void print_diagonal(int n)
 {
 int x, y;
 if (n > 0)
-{
 for (x = 0; x <= n; x++)
 {
 _putchar(' ');
@@ -19,5 +18,4 @@ else if (n <= 0)
 _putchar('\n');
 for (y = 0; y <= x; y++)
 _putchar(' ');
-}
 }
