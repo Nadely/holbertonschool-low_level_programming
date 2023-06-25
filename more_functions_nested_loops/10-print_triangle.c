76b2b087badde5f6b1191448b59e8ec4a543bc6a
Nadely <135656686+Nadely@users.noreply.github.com>
2023-06-25 21:28:19 +0200

    Triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -11,15 +11,14 @@ if (size <= 0)
 _putchar('\n');
 else
 {
-for (x = 1; x <= size; x++)
+for (x = 0; x <= size; x++)
 {
 for (y = 0; y <= size; y++)
 {
 _putchar(35);
 _putchar('\n');
-{
+if (y > x)
 _putchar(' ');
-}
 }
 }
 }
