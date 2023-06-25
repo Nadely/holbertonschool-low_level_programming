cdeea4e4b86ccc9c1c4be82305086e4742b94675
Nadely <135656686+Nadely@users.noreply.github.com>
2023-06-25 12:11:18 +0200

    Fizz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,8 +10,6 @@ for (n = 1; n <= 100; n++)
 {
 if ((n % 3 == 0) && (n % 5 == 0))
 printf("FizzBuzz ");
-else if (n == 100)
-printf("FizzBuzz");
 else if (n % 3 == 0)
 printf("Fizz ");
 else if (n % 5 == 0)
