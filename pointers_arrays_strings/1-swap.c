42602ce381bd0970144adf3c57850529a9af5c0a
Nadely <6890@holbertonschool.com>
2023-06-26 02:48:28 -0700

    swap

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * swap_int - swaps the values
- * Return: swaps the values
- * @a: values
- * @b: values
- */
+* swap_int - swaps the values
+* Return: swaps the values
+* @a: values
+* @b: values
+*/
 void swap_int(int *a, int *b)
 {
-  int c;
-  c = *b;
-  *b = *a;
-  *a = c;
+int c;
+c = *b;
+*b = *a;
+*a = c;
 }
