f3e50bb5b8538fb49f1871873615cc45bb1bd3e9
Nadely <6890@holbertonschool.com>
2023-06-26 08:52:18 -0700

    Puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _puts - followed
+* Return: followed
+* @str: string
+*/
+void _puts(char *str)
+{
+if (*str == 0)
+_putchar('\n');
+else
+{
+_putchar(*str);
+_putchar('\n');
+}
+}
