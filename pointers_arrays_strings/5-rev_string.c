e1cf9232bbeb5920910333f8e74646a736127657
Nadely <6890@holbertonschool.com>
2023-06-27 13:22:15 -0700

    Every other

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -9,6 +9,6 @@ void rev_string(char *s)
 int c;
 for (c = 0; s[c] != '\0'; c++)
 ;
-for (; c >= 0; c--)
+for (c = s['\0'] - 1; c >= s[0]; c--)
 ;
 }
--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -6,3 +6,14 @@
  */
 void puts2(char *str)
 {
+int c;
+if (*str == ' ')
+_putchar('\n');
+else
+for (c = 0; str[c] != '\0'; c++)
+if (str[c] % 2 != 1)
+{
+_putchar(str[c]);
+}
+_putchar('\n');
+}
