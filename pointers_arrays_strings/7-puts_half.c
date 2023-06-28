f52a0355e49b9b846facd522d5e239284581c1e2
Nadely <6890@holbertonschool.com>
2023-06-28 03:10:43 -0700

    Half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -8,11 +8,11 @@ void puts_half(char *str)
 {
 int l, i;
 for (l = 0; str[l] != '\0'; l++)
-if (l % 2 ==1)
+if (l % 2 == 1)
 {
 l++;
 }
-for(i = 0; str[i] != '\0'; i++)
+for (i = 0; str[i] != '\0'; i++)
 {
 if (i >= l / 2)
 _putchar(str[i]);
