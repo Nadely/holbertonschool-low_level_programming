abf5e172ec9ee55a327e16ccd203adb71722315e
Nadely <135656686+Nadely@users.noreply.github.com>
2023-06-28 20:55:01 +0200

    Copies

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -10,12 +10,8 @@ char *_strcpy(char *dest, char *src)
 {
 int i, n = _strlen(src);
 for (i = 0; i < n && src[i] >= '\0'; i++)
-{
 dest[i] = src[i];
-}
 for (; i < n; i++)
-{
 dest[i] = '\0';
-}
 return (dest);
 }
