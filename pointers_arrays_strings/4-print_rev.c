ed551f202095ff5c0a561391c3b0e187c99d8ce6
Nadely <135656686+Nadely@users.noreply.github.com>
2023-06-28 14:19:39 +0200

    Reverse

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -12,12 +12,9 @@ _putchar('\n');
 else
 for (c = 0; s[c] != '\0'; c++)
 ;
-<<<<<<< HEAD
 s[c] = -1;
 for (; c >= 0 ; c--)
-=======
 for (c = -1; c >= 0 ; c--)
->>>>>>> f705a8033a1f8ace42915da7393f739fd8676f3c
 {
 _putchar(s[c]);
 }
