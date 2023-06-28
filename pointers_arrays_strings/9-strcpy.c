03859e42e387cffe00998f36f76225b71dc1306f
Nadely <6890@holbertonschool.com>
2023-06-28 11:26:07 -0700

    Copies

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -8,13 +8,14 @@
 */
 char *_strcpy(char *dest, char *src)
 {
-int i;
-int n = _strlen(src);
+int i, n = _strlen(src);
 for (i = 0; i < n && src[i] != '\0'; i++)
-dest[i] = src[i];
-for (; i < n; i++)
-dest[i] = '\0';
 {
-return (dest);
+dest[i] = src[i];
 }
+for (; i < n; i++)
+{
+dest[i] = '\0';
+}
+return (dest);
 }
