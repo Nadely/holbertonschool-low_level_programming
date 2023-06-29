8429649efce459561f2a7bda2e4ea23e5bcc71d5
Nadely <6890@holbertonschool.com>
2023-06-29 01:31:57 -0700

    Concatenates

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,22 @@
 #include "main.h"
 #include "2-strlen.c"
 /**
- * *_strcat - concatenates
- * Return: concatenates
- * @dest: pointers
- * @src: pointers
- */
+* *_strcat - concatenates
+* Return: concatenates
+* @dest: pointers
+* @src: pointers
+*/
 char *_strcat(char *dest, char *src)
 {
-	int length = _strlen(dest), i;
-	while (dest[length] != '\0')
-	{
-		++length;
-	}
-	for (i = 0; src[i] != '\0'; ++i, ++length)
-	{
-		dest[length] = src[i];
-	}
-	dest[length] = '\0';
-	return (dest);
+int length = _strlen(dest), i;
+while (dest[length] != '\0')
+{
+++length;
+}
+for (i = 0; src[i] != '\0'; ++i, ++length)
+{
+dest[length] = src[i];
+}
+dest[length] = '\0';
+return (dest);
 }
