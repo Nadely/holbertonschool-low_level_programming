5acc72f1e99260a6a9dabbcf9b037f138a2550d6
Nadely <6890@holbertonschool.com>
2023-06-30 01:56:11 -0700

    Copy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,18 @@
 #include "main.h"
 /**
- * *_strncpy - Copy
- * Return: Copy
+* *_strncpy - Copy
+* Return: Copy
+* @dest: pointer
+* @src: pointer
+* @n: num
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+while (dest[n] != '\0')
+{
+src[n] = dest[n];
+n++;
+}
+src[n] = '\0';
+return (0);
+}
