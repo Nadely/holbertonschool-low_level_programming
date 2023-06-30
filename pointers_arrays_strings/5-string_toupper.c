3f0af9422915fe9a5a9067e3e34bd7b0e9937bc7
Nadely <6890@holbertonschool.com>
2023-06-30 13:36:05 -0700

    Change

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- * *string_toupper - Changes
- * Return: Changes
- * @a: pointer
- */
+* *string_toupper - Changes
+* Return: Changes
+* @a: pointer
+*/
 char *string_toupper(char *a)
 {
-	int i;
-	for (i = 0; a[i]; i++)
-	{
-		if (a[i] >= 97 && a[i] <= 122)
-		{
-			a[i] -= 32;
-		}
-	}
-	return (a);
+int i;
+for (i = 0; a[i]; i++)
+{
+if (a[i] >= 97 && a[i] <= 122)
+{
+a[i] -= 32;
+}
+}
+return (a);
 }
