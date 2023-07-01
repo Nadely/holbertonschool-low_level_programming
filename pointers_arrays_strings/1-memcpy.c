51f91ff62d95552789a9961ad97bb67cdb033481
Nadely <6890@holbertonschool.com>
2023-07-01 02:00:10 -0700

    Memory copy

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * *_memcpy - memory copy
- * Return: memory copy
- * @dest: pointer
- * @src: pointer
- * @n: octet
- */
+* *_memcpy - memory copy
+* Return: memory copy
+* @dest: pointer
+* @src: pointer
+* @n: octet
+*/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned a;
-	for (a = 0; a <= n; a++)
-	{
-		dest[a] = src[a];
-	}
-	return (dest);
+unsigned a;
+for (a = 0; a <= n; a++)
+{
+dest[a] = src[a];
+}
+return (dest);
 }
