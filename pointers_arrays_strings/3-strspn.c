3a3bfb348399b6320ed731a50f2f2d59e73aa24b
Nadely <6890@holbertonschool.com>
2023-07-02 06:18:31 -0700

    Gets

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -7,10 +7,10 @@
 */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned i, j;
+unsigned int i, j;
 for (i = 0; s[i] != '\0'; i++)
 {
-for (j =0; accept[j] != s[i]; j++)
+for (j = 0; accept[j] != s[i]; j++)
 {
 if (accept[j] == '\0')
 return (i);
