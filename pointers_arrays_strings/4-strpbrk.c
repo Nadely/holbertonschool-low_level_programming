8b198835be197d6bfdf8a09b006d743bcdd7be0b
Nadely <6890@holbertonschool.com>
2023-07-02 06:37:04 -0700

    Searches

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * *_strpbrk - Searches
+ * Return: Searches
+ * @s: pointer
+ *@accept: pointer
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j, p, f = 0;
+	for (i = 0; s[i] != '\0'; i++)
+		p = i;
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if (accept[i] == s[j])
+			{
+				if(j <= p)
+				{
+					p = j;
+					f = 1;
+				}
+			}
+		}
+	}
+	if (f == 1)
+	{
+		return (&s[p]);
+	}
+	else
+	{
+		return (0);
+	}
+}
