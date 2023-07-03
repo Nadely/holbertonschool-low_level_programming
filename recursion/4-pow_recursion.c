2e4cce93b20286f2658bcfe7cc6d20a87581ec04
Nadely <6890@holbertonschool.com>
2023-07-03 14:05:34 -0700

    Raised

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _pow_recursion - Value and raised
+ * Return: Value and raised
+ * @x: pointer
+ * @y: pointer
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y >= 0)
+	{
+		if (y == 1)
+			return (x);
+		if (y == 0)
+			return (1);
+		return (x *(_pow_recursion(x, y - 1)));
+	}
+	else
+		return (-1);
+}
