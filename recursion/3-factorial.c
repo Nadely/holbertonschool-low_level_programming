3100ee8359fcf1f29b7ddd09676d132297b49cba
Nadely <6890@holbertonschool.com>
2023-07-03 12:52:57 -0700

    Fractional

---
+++ recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * factorial - factorial number
+ * Return: factorial number
+ * @n: numbers
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	else
+		return n * (factorial(n - 1));
+}
