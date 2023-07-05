d5163a361feb2fd2523fb94c0599979a46f71ad9
Nadely <6890@holbertonschool.com>
2023-07-05 13:21:16 -0700

    All

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -7,14 +7,14 @@
 */
 int main(int argc, char *argv[])
 {
-	int i, j;
-	for (i = 0; i < argc; i++)
-	{
-	for (j = 0; argv[i][j] != '\0'; j++)
-	{
-		_putchar(argv[i][j]);
-	}
-	_putchar('\n');
-	}
-	return (0);
+int i, j;
+for (i = 0; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+_putchar(argv[i][j]);
+}
+_putchar('\n');
+}
+return (0);
 }
