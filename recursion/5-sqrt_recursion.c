c0e773b839e950b3e55794552c4c5cd067100b72
Nadely <6890@holbertonschool.com>
2023-07-06 13:20:49 -0700

    Prime ans Square

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -2,31 +2,40 @@
 /**
  * _sqrt_recursion - Natural square root
  * Return: Natural square root
- * @n: numbers
- */
-int _sqrt_recursion(int n)
-{
-	if (n < 0)
-		return (-1);
-	if (n == 0 || n == 1)
-		return (n);
-}
-/**
  * _sqrt - first fonction
  * Return: first fonction
  * @n: numbers
- * @i: number
+ * @i: numbers
+ * @f: numbers
+ * @m: numbers
  */
-int _sqrt(int n, int i, int f, int m)
+int _sqrt_recursion(int n)
 {
-	if (i > f)
-		return (-1);
-	m = (i + f) / 2
+	if(n < 0)
 	{
-	if (m == n / m && n % m == 0)
-		return (m);
-	if (m < n / m)
-		return (_sqrt(n, m + 1, f));
+		return -1;
+	}
+	if(n == 0 || n == 1)
+	{
+		return n;
+	}
+	return _sqrt(n, 1, n);
+}
+int _sqrt (int n, int i, int f)
+{
+	if(i > f)
+	{
+		return -1;
+	}
+	m = (i + f) / 2;
+	if(m == n / m && n % m == 0)
+	{
+		return m;
+	}
+
+	if(m < n / md)
+	{
+		return _sqrt(n, m + 1, f);
 	}
 	return _sqrt(n, i, m - 1);
 }
