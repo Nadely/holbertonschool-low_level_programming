337a7a411edb024a6c3b29f1fdc73c8abc34aa00
Nadely <6890@holbertonschool.com>
2023-07-06 08:57:02 -0700

    Multiplies

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -8,8 +8,7 @@
 */
 int main(int argc, char *argv[])
 {
-	int _atoi(char *s);
-	int i = _atoi(argv[1]), j = _atoi(argv[2]), r = i * j;
+	int i = 0, j = 0, m, r;
 	if (argc != 3)
 	{
 		printf("ERROR\n");
@@ -17,6 +16,15 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
+		for (m = 0; argv[i][m] != '\0'; m++)
+		{
+			i = i * 10 + (argv[i][m] - '0');
+		}
+		for (m = 0; argv[j][m] != '\0'; m++)
+		{
+			j = j * 10 + (argv[j][m] - '0');
+		}
+			r = i * j;
 		printf("%d\n", r);
 		return (0);
 	}
