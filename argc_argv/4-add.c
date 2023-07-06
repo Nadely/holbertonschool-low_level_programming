cd9c89401c756e8fdfa7357d286e53af2ac25221
Nadely <6890@holbertonschool.com>
2023-07-06 12:51:03 -0700

    Adds

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -15,7 +15,7 @@ int r = 0, i, n = 0, j;
 if (argc == 1)
 {
 printf("0\n");
-return 0;
+return (0);
 }
 for (i = 1; i < argc; i++)
 {
@@ -24,12 +24,12 @@ for (j = 0; argv[i][j] != '\0'; j++)
 if (!isdigit(argv[i][j]))
 {
 printf("Error\n");
-return 1;
+return (1);
 }
 }
 n = atoi(argv[i]);
 r += n;
 }
 printf("%d\n", r);
-return 0;
+return (0);
 }
