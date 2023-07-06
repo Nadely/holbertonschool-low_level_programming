4c73f95f880abef72fcc2fd291448a67c95799b3
Nadely <6890@holbertonschool.com>
2023-07-06 12:33:12 -0700

    Adds

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -11,7 +11,12 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, j, r, n = 0;
+	int r = 0, i, n = 0, j;
+	if (argc == 1)
+	{
+		printf("0\n");
+		return 0;
+	}
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
@@ -19,17 +24,13 @@ int main(int argc, char *argv[])
 			if (!isdigit(argv[i][j]))
 			{
 				printf("Error\n");
-				return (1);
+				return 1;
 			}
 			n = n * 10 + (argv[i][j] - '0');
 		}
 		r += n;
 	}
+
 	printf("%d\n", r);
-	return (0);
-	if (argc == 1)
-	{
-		printf("0\n");
-	}
-		return (0);
+	return 0;
 }
