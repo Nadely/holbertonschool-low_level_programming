747533dc72f8c8f32eb640e1703f1656f785e724
Nadely <6890@holbertonschool.com>
2023-07-10 09:48:56 -0700

    Array of chars

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -2,18 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* _strlen - lenght
+ * _strlen - lenght
 * Return: lenght
 * @str: pointer
 */
 unsigned int _strlen(char *str)
 {
-	unsigned int l;
+unsigned int l;
 for (l = 0; str[l] != '\0'; l++)
 {
-	continue;
+continue;
 }
-	return (l);
+return (l);
 }
 /**
 * _strdup - newly allocated
@@ -22,21 +22,21 @@ for (l = 0; str[l] != '\0'; l++)
 */
 char *_strdup(char *str)
 {
-	unsigned int i;
-	char *array;
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-	array = malloc((_strlen(str) + 1) * sizeof(char));
-	if (array != NULL)
-	{
-		for (i = 0; str[i] != '\0'; i++)
-		{
-			array[i] = str[i];
-		}
-		array[i] = '\0';
-		return (array);
-	}
-	return (NULL);
+unsigned int i;
+char *array;
+if (str == NULL)
+{
+return (NULL);
+}
+array = malloc((_strlen(str) + 1) * sizeof(char));
+if (array != NULL)
+{
+for (i = 0; str[i] != '\0'; i++)
+{
+array[i] = str[i];
+}
+array[i] = '\0';
+return (array);
+}
+return (NULL);
 }
