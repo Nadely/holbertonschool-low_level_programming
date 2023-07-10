972b212bc5f5c8878513de1c4d4f3477be56487e
Nadely <6890@holbertonschool.com>
2023-07-10 02:52:18 -0700

    Array of chars

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,7 +12,7 @@ char *create_array(unsigned int size, char c)
 	char *array = (char *)malloc(size * sizeof(char));
 	if (array != NULL)
 	{
-		for(i = 0; i < size; i++)
+		for (i = 0; i < size; i++)
 		{
 			array[i] = c;
 		}
