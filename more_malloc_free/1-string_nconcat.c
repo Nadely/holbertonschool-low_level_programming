a9701756718e862adb7ac05bd1f269992f6a490d
Nadely <6890@holbertonschool.com>
2023-07-11 06:29:30 -0700

    Concatenates two strings

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		return ("");
 	if (n >= m)
-		n = m);
+		n = m;
 	array = malloc((l + n + 1) * sizeof(char));
 	if (array == NULL)
 		return (NULL);
