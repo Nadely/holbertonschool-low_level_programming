7cc80b1691ba5fbde2b197d5d69a50d592e264ca
Nadely <6890@holbertonschool.com>
2023-07-12 05:06:52 -0700

    Computes

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,5 +1,5 @@
-#ifndef ABS_HEADER
-#define ABS_HEADER
+#ifndef _ABS_HEADER
+#define _ABS_HEADER
 #ifndef ABS
 #define ABS(x) ((x) < 0 ? ((x) * (-1)) : (x))
 #endif
---
+++ preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef _SUM_HEADER
+#define _SUM_HEADER
+#ifndef SUM
+#define SUM(x, y) ((x) + (y))
+#endif
+#endif
