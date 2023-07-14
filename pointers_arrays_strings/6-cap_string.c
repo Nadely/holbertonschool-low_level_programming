f59573dd3d1a5e47ab1a8fe2d989f3076db33a9a
Nadely <6890@holbertonschool.com>
2023-07-14 02:22:52 -0700

    Cap

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -6,4 +6,24 @@
  */
 char *cap_string(char *s)
 {
-	
+	int i = 0;
+	int capitalize = 1;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			if (capitalize)
+			{
+				s[i] -= 32;
+				capitalize = 0;
+			}
+		}
+		else if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?' || s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}')
+		{
+			capitalize = 1;
+		}
+		i++;
+	}
+	return (s);
+}
