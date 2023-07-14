47a138923627fd3358339921815dd9bd6038253f
Nadely <6890@holbertonschool.com>
2023-07-14 06:13:44 -0700

    Print triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -6,17 +6,21 @@
 */
 void print_triangle(int size)
 {
-int x, y;
-if (size <= 0)
-_putchar('\n');
-else
-{
-for (y = 0; y <= size; y++)
-{
-for (x = 1; x <= size; x++)
-if (y < x)
-_putchar(35);
-_putchar('\n');
-}
-}
+	int x, y;
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (y = size; y > 0; y--)
+	{
+		for (x = 1; x <= size; x++)
+		{
+			if (x >= y)
+				_putchar('#');
+			else
+				_putchar(' ');
+		}
+		_putchar('\n');
+	}
 }
