5ac08d2f2babaa4fd09b65d118712b26811901ec
Nadely <6890@holbertonschool.com>
2023-07-14 06:04:32 -0700

    Print diagonal

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,4 +1,4 @@
- #include "main.h"
+#include "main.h"
 /**
 * print_diagonal - diagonal line
 * Return: diagonal line
@@ -6,21 +6,22 @@
 */
 void print_diagonal(int n)
 {
-int x, y;
-{
-if (n <= 0)
-_putchar('\n');
-else
-{
-for (x = 1; x <= n; x++)
-{
-for (y = 1; y <= x; y++)
-_putchar(' ');
-{
-_putchar(92);
-_putchar ('\n');
-}
-}
-}
-}
+	int x, y;
+
+	if (n <= 0)
+	{
+	_putchar('\n');
+	return;
+	}
+	for (x = 1; x <= n; x++)
+	{
+		for (y = 1; y <= x; y++)
+		{
+			if ( y == x)
+			_putchar('\\');
+			else
+			_putchar(' ');
+		}
+			_putchar ('\n');
+	}
 }
