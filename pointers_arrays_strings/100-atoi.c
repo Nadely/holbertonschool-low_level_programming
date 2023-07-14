00f45a8c291907f1060b94007e666a02e8ee939d
Nadely <6890@holbertonschool.com>
2023-07-14 03:36:14 -0700

    atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 /**
  * _atoi - fonction
  * Return: fonction
@@ -6,30 +7,35 @@
  */
 int _atoi(char *s)
 {
+	if (s == 0)
+		return (0);
 	int signe = 1;
 	int resultat = 0;
 	int i = 0;
 
-	if (s[0] == '-')
+	while (s[i] == '-' || s[i] == '+')
 	{
-		signe = -1;
+		if (s[i] == '-')
+			signe *= -1;
 		i++;
 	}
-	else if (s[0] == '+')
-	{
-		i++;
-	}
-	for (; s[i] != '\0'; i++)
+	while (s[i] != '\0')
 	{
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			int chiffre = s[i] - '0';
+			if (resultat > (INT_MAX - chiffre) / 10)
+			{
+				if (signe == 1)
+					return (INT_MAX);
+				else
+					return (INT_MIN);
+			}
 			resultat = resultat * 10 + chiffre;
 		}
 		else
-		{
 			break;
-		}
+		i++;
 	}
 	return (signe * resultat);
 }
