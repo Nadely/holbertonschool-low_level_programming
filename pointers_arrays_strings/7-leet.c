390b1778a021292b051a0cb93ec8da5245b8c719
Nadely <6890@holbertonschool.com>
2023-07-14 02:46:42 -0700

    leet

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -9,11 +9,15 @@ char *leet(char *s)
 	int i = 0, j;
 	char t[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 	char u[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+
 	while (s[i])
 	{
 		for (j = 0; j < 10; j++)
-			if (s[i] == t[j])
-				(s[i] = u[j]);
+			if (s[i] == u[j])
+			{
+				[s[i] = t[j]);
+				break;
+			}
 		i++;
 	}
 	return (s);
