398a2b343932cd2e1b0b492c43ce4c3198eadf7a
Nadely <6890@holbertonschool.com>
2023-07-14 07:19:13 -0700

    Print to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -6,8 +6,33 @@
  */
 void print_to_98(int n)
 {
-  int x;
-  for(x = n; x <= 98; x++)
-    _putchar(x)
-  _putchar('\n')
-    }
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	_putchar('\n');
+}
