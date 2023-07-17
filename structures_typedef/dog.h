26bddd126d4420265445803db75e25342189df4f
Nadely <6890@holbertonschool.com>
2023-07-17 01:49:38 -0700

    My first struct

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - define a struct for a main.c
+ * @name: name dog
+ * @age: age dog
+ * @owner: who
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
