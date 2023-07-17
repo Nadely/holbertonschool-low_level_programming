526d530448b0d98709a30c2982f89ccae661604d
Nadely <6890@holbertonschool.com>
2023-07-17 09:25:44 -0700

    Fuction that searches

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -17,6 +17,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	if (size <= 0)
 		return (-1);
+	if (array == NULL)
+		return (-1);
 
 	for (size_of = 0; size_of < size; size_of++)
 	{
