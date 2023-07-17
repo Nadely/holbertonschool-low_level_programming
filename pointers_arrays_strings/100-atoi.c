0e2fbc2e7ca8043d14b9791b489085c88fd1d0d8
Nadely <135656686+Nadely@users.noreply.github.com>
2023-07-17 21:38:33 +0200

    Atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -18,7 +18,7 @@ int _atoi(char *s)
 	}
 	while (s[i] != '\0' && s[i] >= '0' && s[i] <= '9')
 	{
-		result = result * 10 + s[i] – '0';
+		result = result * 10 + s[i] - '0';
 		i++;
 	}
 	return ( result * sign );
