80fa79dd7921aba428d9a5437aee9d9847972b48
Nadely <6890@holbertonstudents.com>
2023-07-18 15:53:36 +0200

    program that performs simple operations

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -12,24 +12,30 @@
 int main(int argc, char **argv)
 {
 	int result, num1, num2;
-	int (*operation)(int, int);
+	char *operation;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
-		return (argc);
+		return (98);
 	}
 	num1 = atoi(argv[1]);
+	operation = argv[2];
 	num2 = atoi(argv[3]);
-
-	operation = get_op_func(argv[2]);
 
 	if (!operation)
 	{
 		printf("Error\n");
-		return (!operation);
+		return (99);
 	}
-	result = operation(num1, num2);
+
+	if((*operation == '/' || *operation == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+
+	result = get_op_func(operation)(num1, num2);
 	printf("%d\n", result);
 	return (EXIT_SUCCESS);
 }
