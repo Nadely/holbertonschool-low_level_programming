abe091ae3096082437c40828ba99bbe246534249
Nadely <6890@holbertonstudents.com>
2023-07-18 18:02:09 +0200

    function that prints numbers

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -12,9 +12,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list complet; /* Initialize the argument list*/
 	unsigned int number;
 
-	if (separator == NULL)
-		printf("%s", separator);
-
 	va_start(complet, n); /* Get the next argument value*/
 
 	for (number = 0; number < n; number++)
