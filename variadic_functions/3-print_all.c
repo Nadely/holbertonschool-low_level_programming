58daa22b8d2cee313bc5e869d55eca8459b9c212
Nadely <6890@holbertonstudents.com>
2023-07-20 20:22:15 +0200

    Print all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,12 +2,88 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 
+/*
+* printf_s - print string
+* @all: va_list argument
+* Return: print string
+*/
+void printf_s (va_list all)
+{
+	char *s = va_arg(all, char *);
+	if (s == NULL)
+	printf("(nil)");
+	else
+	printf("%s", s);
+}
+
+/*
+* printf_c - print char
+* @all: va_list argument
+* Return: print char
+*/
+void printf_c (va_list all)
+{
+	char c = va_arg(all, int);
+	printf("%c", c);
+}
+
+/*
+* printf_i - print int
+* @all: va_list argument
+* Return: print int
+*/
+void printf_i (va_list all)
+{
+	int i = va_arg(all, int);
+	printf("%d", i);
+}
+
+/*
+* printf_f - print float
+* @all: va_list argument
+* Return: print float
+*/
+void printf_f (va_list all)
+{
+	double f = va_arg(all, double);
+	printf("%f", f);
+}
+
 /**
  * print_all - Function that prints anything
- * @char: const
- * @format: const int
+ * @format: const char
+ * Return: Function that print anything
 */
 void print_all(const char * const format, ...)
 {
+	pri_t _print [] = {
+		{"s", printf_s},
+		{"c", printf_c},
+		{"i", printf_i},
+		{"f", printf_f},
+	};
+	va_list all;
+	int i = 0, j;
+	char *separator = "";
 
+	va_start(all, format);
+
+	while (format && format[i]/* condition */)
+	{
+		j = 0;
+		while (j < 4/* condition */)
+		{
+			if (format[i] == *(_print[j].print))
+			{
+				printf ("%s", separator);
+				_print[j].func(all);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+
+	va_end(all);
+	printf("\n");
 }
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,10 @@
 #ifndef PROTECT
 #define PROTECT
+typedef struct _print
+{
+	char *print;
+	void (*func)(va_list);
+}pri_t;
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
