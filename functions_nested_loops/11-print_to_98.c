90d86e341ffc0c5d842c8160a75dbb126661dfec
Nadely <6890@holbertonstudents.com>
2023-07-21 23:08:51 +0200

    Print 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -6,33 +6,35 @@
  */
 void print_to_98(int n)
 {
-	int i;
-
-	if (n <= 98)
+	while (n != 98)
 	{
-		for (i = n; i <= 98; i++)
+		if (n <= 999 && n >= 100)
 		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar('0' + (n / 100 % 10));
+			_putchar('0' + (n / 10 % 10));
 		}
-	}
-	else
-	{
-		for (i = n; i >= 98; i--)
+		else if (n > 9 && n <= 99)
+		_putchar('0' + (n / 10 % 10));
+		if (n < 0)
 		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar('-');
+			if (n < -99)
+			_putchar('0' + ((n * -1) / 100) % 10);
+			if (n < -9)
+			_putchar('0' + ((n * -1) / 10) % 10);
 		}
+		if (n >= 0)
+		_putchar('0' + n % 10);
+		if (n < 0)
+		_putchar('0' + ((n * -1) % 10));
+		_putchar(',');
+		_putchar(' ');
+		if (n < 98)
+		n++;
+		if (n > 98)
+		n--;
 	}
+	_putchar('9');
+	_putchar('8');
 	_putchar('\n');
 }
