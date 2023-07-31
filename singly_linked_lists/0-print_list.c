1e87fbd03a49ceda8053d6cede6a28d7ab61a3e3
Nadely <6890@holbertonstudents.com>
2023-07-31 13:26:32 +0200

    Print all the element

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints all the element
+ * @h: Pointer Struct list
+ *
+ * Description: singly linked list node structure
+ * Return: print all the element
+ */
+
+size_t print_list(const list_t *h)
+{
+	int size = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+			size++;
+		}
+		else
+		{
+			printf("[%u] ", h->len);
+			printf("%s\n", h->str);
+			h = h->next;
+			size++;
+		}
+	}
+	return (size);
+}
