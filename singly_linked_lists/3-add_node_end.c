0f5583d705598f22a5f6066a33cf4a6010611f33
Nadely <6890@holbertonstudents.com>
2023-07-31 15:51:42 +0200

    Add a new mode at the end

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -13,34 +13,34 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *end = *head;
-	list_t *new_add_node = (list_t *)malloc(sizeof(list_t));
+	list_t *end = *head; /*Pointer to track the end of the list */
+	list_t *new_add_node = (list_t *)malloc(sizeof(list_t)); /*Allocate memory for new node*/
 
-	if (head == NULL)
+	if (head == NULL) /* Check if the head pointer is NULL*/
 		return (NULL);
 
-	if (new_add_node == NULL)
+	if (new_add_node == NULL) /*Check if memory allocation failed*/
 		return (NULL);
 
-	new_add_node->str = strdup(str);
-	if (new_add_node->str == NULL)
+	new_add_node->str = strdup(str); /*Duplicate the string and assign it to the new node's str*/
+	if (new_add_node->str == NULL) /*Check if strdup failed*/
 	{
 		free(new_add_node);
 		return (NULL);
 	}
 
-	new_add_node->len = strlen(str);
-	new_add_node->next = NULL;
+	new_add_node->len = strlen(str); /*Determine the length of the string and assign it to the new node's len */
+	new_add_node->next = NULL; /* Set the next pointer of the new node to NULL*/
 
-	if (*head == NULL)
+	if (*head == NULL) /*If the list is empty, make the new node the head*/
 	{
 		*head = new_add_node;
 		return (new_add_node);
 	}
 
-	while (end->next != NULL)
+	while (end->next != NULL) /*Traverse the list to reach the end*/
 	end = end->next;
-	end->next = new_add_node;
+	end->next = new_add_node; /*Assign the new node as the next node of the last node*/
 
-	return (new_add_node);
+	return (new_add_node); /* Return the address of the new node*/
 }
