e05fc9ef8b525d0a274e1a5a29c765e46d3e2d8d
Nadely <6890@holbertonstudents.com>
2023-07-31 16:42:01 +0200

    Free

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * free_list - Free
+ * @head: Pointer to pointer
+ *
+ * Description: new node
+ * Return: Free
+ */
+
+void free_list(list_t *head)
+{
+	list_t *node = head;
+	list_t *next;
+
+	if (node != NULL)
+	{
+		next = node->next;
+		free(node->str);
+		free(node);
+		node = next;
+	}
+}
