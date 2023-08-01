9cdcfd2743bcd4ffb31864de558af45deb33687d
Nadely <6890@holbertonstudents.com>
2023-08-01 18:30:18 +0200

    correction inserts a new node at a given position

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -20,11 +20,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (new_node == NULL)
 		return (NULL);
-
 	new_node->n = n;
 	new_node->next = NULL;
 	new_node->prev = NULL;
-
 	if (*h == NULL)
 	{
 		*h = new_node;
@@ -33,16 +31,20 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new_node->next = *h;
+		(*h)->prev = new_node;
 		*h = new_node;
 		return (new_node);
 	}
-
 	while (node != NULL && i < idx - 1)
 	{
 	node = node->next;
 	i++;
 	}
-
+	if (node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 	new_node->next = node->next;
 	if (node->next != NULL)
 		node->next->prev = new_node;
