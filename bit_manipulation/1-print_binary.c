eae1d957d0628324ea2c04662fdb01b153961b0a
Nadely <6890@holbertonstudents.com>
2023-08-02 14:26:07 +0200

    prints the binary representation of a number

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -19,13 +19,11 @@ void print_binary(unsigned long int n)
 	}
 
 	for (i = sizeof(n) * 8 - 1; i >= 0; i--) /*sizeof(n) * 8 - 1 : Cette partie du code utilise l'opérateur sizeof pour obtenir la taille en octets de la variable n et le multiplie par 8 pour obtenir la taille en bits. Ensuite, on soustrait 1 pour obtenir le numéro du bit le plus significatif (le bit à l'indice le plus élevé)*/
-	if ((n >> i) & 1) /*si le résultat de cette comparaison est vrai (c'est-à-dire que le bit le plus à droite est un 1), alors l'expression ((n >> i) & 1) retournera un résultat de 1. Sinon, elle retournera un résultat de 0*/
-	{
-		_putchar('1');
-		f = 1; /*Définir le drapeau pour indiquer que les zéros initiaux doivent être imprimés*/
-	}
-	else if (f) /*Si l'indicateur est activé, imprimer 0*/
-	_putchar ('0');
+		if ((n >> i) & 1)					 /*si le résultat de cette comparaison est vrai (c'est-à-dire que le bit le plus à droite est un 1), alors l'expression ((n >> i) & 1) retournera un résultat de 1. Sinon, elle retournera un résultat de 0*/
+		{
+			_putchar('1');
+			f = 1; /*Définir le drapeau pour indiquer que les zéros initiaux doivent être imprimés*/
+		}
+		else if (f) /*Si l'indicateur est activé, imprimer 0*/
+			_putchar('0');
 }
-
-
