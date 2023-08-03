c00c87cb31fc81640edce22c9e13ab3d18f26aa1
Nadely <6890@holbertonstudents.com>
2023-08-03 15:37:51 +0200

    returns the number of bits

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * flip_bits - returns the number of bits
+ * @n: number
+ * @m: number
+ *
+ * Return:returns the number of bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int d = n ^ m; /*trouver les bits différent entre les 2*/
+	unsigned int count = 0;
+
+	while (d > 0)
+	{
+		/*on verifie si le bits est différent*/
+		if (d & 1)
+		count++; /*si oui on incémente*/
+
+		d >>=1;
+		/*on décale le nombre pour voir s'il est différent avec le suivant*/
+	}
+	return (count); /* on revoie le nombre de bits différent en n et m*/
+}
