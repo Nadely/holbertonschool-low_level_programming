2b3d16593d613afe3311236d42f3f45c7cb12f38
Nadely <6890@holbertonstudents.com>
2023-08-03 15:47:07 +0200

    correction returns the number of bits

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -10,8 +10,8 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int d = ((n ^ m) * 2); /*trouver les bits différent entre les 2*/
-	unsigned int count = 0;
+	unsigned long int d = n ^ m; /*trouver les bits différent entre les 2*/
+	unsigned long int count = 0;
 
 	while (d > 0)
 	{
