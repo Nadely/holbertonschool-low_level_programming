f16e92ad82108e689043592c4acde01a5ab46a33
Nadely <6890@holbertonstudents.com>
2023-08-07 13:15:33 +0200

    2nd correction 1. Under the snow

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -17,8 +17,8 @@
 int create_file(const char *filename, char *text_content)
 {
 	int file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	int len = strlen(text_content);
-	int write_file = write(file, text_content, len);
+	int len = 0;
+	int write_file = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -26,13 +26,19 @@ int create_file(const char *filename, char *text_content)
 	if (file == -1)
 		return (-1);
 
+	if (text_content != NULL)
+	{
+		len = strlen(text_content);
+		write_file = write(file, text_content, len);
+	}
+	close(file);
+
 	if (write_file == -1 || write_file != len)
 	{
 		close(file);
 		return (-1);
 	}
 
-	if (text_content != NULL)
 	close(file);
 	return (1);
 }
