9980e9aad049478b9f966a771ed204899ccb66a9
Nadely <6890@holbertonstudents.com>
2023-08-07 11:48:59 +0200

    0. Tread lightly, she is near (commentaires)

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -15,23 +15,30 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file = open(filename, O_RDONLY);
+	/* Ouverture du fichier en mode lecture seule*/
 	char *memory = malloc(letters * sizeof(char));
+	/*Allocation de mémoire pour stocker les caractères lus*/
 	int read_file = read(file, memory, letters);
+	/*Lecture de 'letters' caractères à partir du fichier*/
 	int write_file = write(STDOUT_FILENO, memory, read_file);
+	/*Écriture des caractères lus sur la sortie standard*/
 
 	if (filename == NULL)
 		return (0);
 
 	if (file == -1)
+	/*Si une erreur s'est produite lors de l'ouverture du fichier*/
 		return (0);
 
 	if (memory == NULL)
+	/*Si une erreur s'est produite lors de l'allocation de mémoire*/
 	{
 		close(file);
 		return (0);
 	}
 
 	if (read_file <= 0)
+	/* Si une erreur s'est produite lors de la lecture du fichier*/
 	{
 		close(file);
 		free(memory);
@@ -39,6 +46,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	if (write_file <= 0 || write_file != read_file)
+	/*Si une erreur s'est produite lors de l'écriture sur la sortie standard*/
 	{
 		close(file);
 		free(memory);
