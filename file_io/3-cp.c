d7dc38d4cd09c6ba4a17a47cca6e93ad225c6ffc
Nadely <6890@holbertonstudents.com>
2023-08-07 15:41:09 +0200

    first correction 3. cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -24,26 +24,26 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage : %s file_from file_to\n", argv[0]);
-			exit(97);
+		exit(97);
 	}
 
 	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
+		exit(98);
 	}
 
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
-			exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
 	}
 
 	while (read_file > 0)
 	{
 		if (write_file == -1 || write_file < read_file)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
@@ -51,14 +51,14 @@ int main(int argc, char *argv[])
 	if (read_file == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
+		exit(98);
 	}
 
 	if (close(file_from) == -1 || close(file_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error : Can't close file\n");
-			exit(100);
+		exit(100);
 	}
 
-	return (0);
+	return (100);
 }
