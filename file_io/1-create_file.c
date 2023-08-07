dbc69e1718663fa00c97e0cc26e0bd8855ebb50b
Nadely <6890@holbertonstudents.com>
2023-08-07 13:09:04 +0200

    correction 1. Under the snow

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
 
 /**
 * create_file - Create a function that creates a file
@@ -15,7 +17,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	int len = 0;
+	int len = strlen(text_content);
 	int write_file = write(file, text_content, len);
 
 	if (filename == NULL)
@@ -29,9 +31,6 @@ int create_file(const char *filename, char *text_content)
 		close(file);
 		return (1);
 	}
-
-	while (text_content[len])
-		len++;
 
 	if (write_file == -1 || write_file != len)
 	{
