53fc4c72b1b85e44fa53f458c01851254b219f61
Nadely <6890@holbertonstudents.com>
2023-08-07 17:09:10 +0200

    7 correction 3. cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 #include <fcntl.h>
 
 /**
@@ -16,49 +15,42 @@
 
 int main(int argc, char *argv[])
 {
-	char temp[1024];
-	int file_from, file_to, read_file, write_file;
+	char buffer[1024];
+	int file_from, file_to, bytes_read, bytes_written;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1)
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file_from == -1 || file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		if (file_from != -1)
+			close(file_from);
+		dprintf(STDERR_FILENO, "Error: Can't open or create file\n");
 		exit(98);
 	}
-
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (file_to == -1)
+	while ((bytes_read = read(file_from, buffer, sizeof(buffer))) > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-
-	while ((read_file = read(file_from, temp, sizeof(temp))) > 0)
-	{
-		write_file = write(file_to, temp, read_file);
-		if (write_file == -1 || write_file != read_file)
+		bytes_written = write(file_to, buffer, bytes_read);
+		if (bytes_written == -1 || bytes_written != bytes_read)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(file_from);
+			close(file_to);
+			dprintf(STDERR_FILENO, "Error: Can't write to file\n");
 			exit(99);
 		}
 	}
-
-	if (read_file == -1)
+	if (bytes_read == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close(file_from);
+		close(file_to);
+		dprintf(STDERR_FILENO, "Error: Can't read from file\n");
 		exit(98);
 	}
-
-	if (close(file_from) == -1 || close(file_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd\n");
-		exit(100);
-	}
+	close(file_from);
+	close(file_to);
 	return (0);
 }
