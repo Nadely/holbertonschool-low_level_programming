478c092655908dfc21201719ea6414341477b6af
Nadely <6890@holbertonstudents.com>
2023-08-08 15:22:01 +0200

    Betty correction 3. cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -12,20 +12,18 @@
 int main(int argc, char *argv[])
 {
 	char temp[1024];
-	int file_from, file_to, read_file, write_file;
+	int file_from = open(argv[1], O_RDONLY), file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664), read_file, write_file;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
