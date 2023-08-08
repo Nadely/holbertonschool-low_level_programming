a0ec62985ffdc09e628a3debc39bad1ef7f4b55a
Nadely <6890@holbertonstudents.com>
2023-08-08 16:46:26 +0200

    0. >>> ht = {}

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *hash_table_create - pointer of hash
+ * @size = size of the array
+ * Return:  that creates a hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table = NULL;
+	unsigned long int i;
+
+	if (size < 1) /*Vérification de la taille*/
+		return (NULL);
+
+	if ((hash_table = malloc(sizeof(hash_table_t))) == NULL)
+		/*Allocation de mémoire pour la structure de la table de hachage*/
+		return (NULL);
+
+	if ((hash_table->array = malloc(sizeof(hash_node_t *) * size)) == NULL)
+		/*Allocation de mémoire pour le tableau de pointeurs*/
+		free(hash_table);
+	return (NULL);
+
+	for (i = 0; i < size; i++)
+	/*Initialisation des pointeurs à NULL*/
+	{
+		hash_table->array[i] = NULL;
+	}
+
+	/*Attribution de la taille à la structure*/
+	hash_table->size = size;
+
+	return (hash_table);
+}
