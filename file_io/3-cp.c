f9347a8e6485eee8bb66f96c4620c37f9bf2ba51
Nadely <6890@holbertonstudents.com>
2023-08-08 15:12:32 +0200

    Betty correction 3. cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-
 /**
  * main - copy file
  * @argc: argument
@@ -12,24 +11,22 @@
  *
  * Return: copy file
  */
-
 int main(int argc, char *argv[])
 {
 	char temp[1024];
-	int file_from, file_to, read_file, write_file;
+	int file_from = open(argv[1], O_RDONLY), file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	int read_file, write_file = write(file_to, temp, read_file);
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
@@ -37,7 +34,6 @@ int main(int argc, char *argv[])
 	}
 	while ((read_file = read(file_from, temp, sizeof(temp))) > 0)
 	{
-		write_file = write(file_to, temp, read_file);
 		if (write_file == -1 || write_file != read_file)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
