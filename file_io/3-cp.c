2e37417aa4b07c6359392d701b2f39a25393e6ff
Nadely <6890@holbertonstudents.com>
2023-08-08 15:04:12 +0200

    close correction 3. cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -49,9 +49,14 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (close(file_from) == -1 && close(file_to) == -1)
+	if (close(file_from) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd\n");
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	if (close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
 		exit(100);
 	}
 	return (0);
