8797177abe2d4ac580bc45f56624bc0ff95b1ed0
Nadely <6890@holbertonstudents.com>
2023-08-08 15:25:09 +0200

    Betty correction 3. cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -9,28 +9,25 @@
  * @argv: double pointer
  * Return: copy file
  */
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	char temp[1024];
-	int file_from = open(argv[1], O_RDONLY), file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664), read_file, write_file;
+	int file_from, file_to, read_file, write_file;
 
-	if (argc != 3)
-	{
+	if (argc != 3) {
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	if (file_from == -1)
-	{
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1) {
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (file_to == -1)
-	{
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file_to == -1) {
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	while ((read_file = read(file_from, temp, sizeof(temp))) > 0)
-	{
+	while ((read_file = read(file_from, temp, sizeof(temp))) > 0) {
 		write_file = write(file_to, temp, read_file);
 		if (write_file == -1 || write_file != read_file)
 		{
@@ -38,18 +35,15 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 	}
-	if (read_file == -1)
-	{
+	if (read_file == -1) {
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (close(file_from) == -1)
-	{
+	if (close(file_from) == -1) {
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
-	if (close(file_to) == -1)
-	{
+	if (close(file_to) == -1) {
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
 		exit(100);
 	}
