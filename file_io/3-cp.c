d98b36f1a18a9fb0a64846b407640695cd077d99
Nadely <6890@holbertonstudents.com>
2023-08-08 15:20:09 +0200

    Betty correction 3. cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -7,33 +7,33 @@
  * main - copy file
  * @argc: argument
  * @argv: double pointer
- * Description: copy file
- *
  * Return: copy file
  */
 int main(int argc, char *argv[])
 {
 	char temp[1024];
-	int file_from = open(argv[1], O_RDONLY), file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	int read_file = read(file_from, temp, sizeof(temp)), write_file = write(file_to, temp, read_file);
+	int file_from, file_to, read_file, write_file;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	while (read_file > 0)
+	while ((read_file = read(file_from, temp, sizeof(temp))) > 0)
 	{
+		write_file = write(file_to, temp, read_file);
 		if (write_file == -1 || write_file != read_file)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
