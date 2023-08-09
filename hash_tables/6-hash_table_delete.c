14f2089b3d323894830f777b5348f1574b342622
Nadely <6890@holbertonstudents.com>
2023-08-09 23:08:31 +0200

    correction 6. >>> del ht

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -4,19 +4,32 @@
 #include <string.h>
 
 /**
- * hash_table_print - deletes a hash table
+ * hash_table_delete - deletes a hash table
  * @ht: the hash table
  * Return: deletes a hash table
  */
 
 void hash_table_delete(hash_table_t *ht)
 {
-	if (ht == NULL)
-		free(ht);
+	unsigned long int i;
+	hash_node_t *current;
+	hash_node_t *next;
 
-	else
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
 	{
-		free(ht->array);
-		free(ht);
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			next = current->next;
+			free(current->key);
+			free(current->value);
+			free(current);
+			current = next;
+		}
 	}
+	free(ht->array);
+	free(ht);
 }
